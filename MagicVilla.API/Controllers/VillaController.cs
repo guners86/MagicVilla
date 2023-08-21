57ff72cbc44f00ff4ff79ad81bd4808f652f2b12
Damian Arturo Garces Paez <guners86@hotmail.com>
2023-08-20 19:29:47 -0500

    Add Villa controller

---
+++ MagicVilla.API/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla.API.Models.DataTransferObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return new List<VillaDto>
+            {
+                new VillaDto { Id = 1, Name = "Vista a la Piscina" },
+                new VillaDto { Id = 2, Name = "Vista a la Playa" }
+            };
+        }
+    }
+}
---
+++ MagicVilla.API/Models/DataTransferObjects/VillaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla.API.Models.DataTransferObjects
+{
+    public class VillaDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
